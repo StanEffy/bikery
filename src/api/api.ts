d4cb1a71f9578315d6859b261189f116da517ffb
Stan_Effy <47476119+StanEffy@users.noreply.github.com>
2022-08-11 12:47:39 +0300

    add api with axios

---
+++ src/api/api.ts
@@ -0,0 +1,14 @@
+
+import axios from 'axios';
+
+export const apiStations = axios.create({
+    baseURL: '/',
+    // @ts-ignore
+    'Access-Control-Allow-Credentials': true
+})
+
+export const apiTrips = axios.create({
+    baseURL: '/',
+    // @ts-ignore
+    'Access-Control-Allow-Credentials': true
+})
