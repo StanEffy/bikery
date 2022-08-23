1b40d213a6dd44c233dbf1b5bba80a49919e2fc6
Stan_Effy <47476119+StanEffy@users.noreply.github.com>
2022-08-23 19:00:32 +0300

    add eslint and prettier

--- src/components/JourneysList/JourneysFilters.tsx
+++ src/components/JourneysList/JourneysFilters.tsx
@@ -1,107 +1,107 @@
-import React, {useState} from 'react';
-import Checkbox from '@mui/material/Checkbox';
-import TextField from '@mui/material/TextField';
-import Autocomplete from '@mui/material/Autocomplete';
-import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
-import CheckBoxIcon from '@mui/icons-material/CheckBox';
-import {useSelector} from "react-redux";
+import React, {useState} from "react"
+import Checkbox from "@mui/material/Checkbox"
+import TextField from "@mui/material/TextField"
+import Autocomplete from "@mui/material/Autocomplete"
+import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank"
+import CheckBoxIcon from "@mui/icons-material/CheckBox"
+import {useSelector} from "react-redux"
 import {Station} from "../../store/actions/types"
 import {
-    Box,
-    FormControlLabel,
-    InputAdornment,
-} from "@mui/material";
+	Box,
+	FormControlLabel,
+	InputAdornment,
+} from "@mui/material"
 
 // @ts-ignore
 export const JourneysStationFilters = ({handleFilters}) => {
-    // @ts-ignore
-        const allStations = useSelector(state => state.stations.allStations)
+	// @ts-ignore
+	const allStations = useSelector(state => state.stations.allStations)
 
-    const handleStationChange = (value: any) => {
-        handleFilters((prevState: any) => ({...prevState, departure_station_id: value}))
-    };
+	const handleStationChange = (value: any) => {
+		handleFilters((prevState: any) => ({...prevState, departure_station_id: value}))
+	}
 
-        return (
-            <Autocomplete
-                disablePortal
-                id="combo-box-demo"
-                options={allStations}
-                onChange={(event, value) => {
-                    handleStationChange(value)
-                }}
-                sx={{ width: 300 }}
-                getOptionLabel={(option:Station) => option.Name}
-                renderOption={(props, option) => (
-                    <Box component="li" sx={{ '& > img': { mr: 2, flexShrink: 0 } }} {...props}>
-                        {option.Name}
-                    </Box>
-                )}
-                    renderInput={(params) => <TextField {...params} label="Departure station" />}
-            />
-        );
-    }
+	return (
+		<Autocomplete
+			disablePortal
+			id="combo-box-demo"
+			options={allStations}
+			onChange={(event, value) => {
+				handleStationChange(value)
+			}}
+			sx={{ width: 300 }}
+			getOptionLabel={(option:Station) => option.Name}
+			renderOption={(props, option) => (
+				<Box component="li" sx={{ "& > img": { mr: 2, flexShrink: 0 } }} {...props}>
+					{option.Name}
+				</Box>
+			)}
+			renderInput={(params) => <TextField {...params} label="Departure station" />}
+		/>
+	)
+}
 
 // @ts-ignore
 export const DistanceFilter = ({handleFilters}) => {
-    const [distance, setDistance] = useState("0")
-    const [checked, setChecked] = React.useState(true);
+	const [distance, setDistance] = useState("0")
+	const [checked, setChecked] = React.useState(true)
 
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setChecked(event.target.checked);
-        handleFilters((prevState: any) => ({...prevState, distance_is_greater: event.target.checked}))
-    };
-    const handleDistanceChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setDistance(event.target.value);
-        handleFilters((prevState: any) => ({...prevState, covered_distance_m: event.target.value}))
-    };
-    return (
-        <Box display={"flex"} alignItems={"center"}>
-            <FormControlLabel control={<Checkbox checked={checked} onChange={handleChange}/>} label={checked ? "Greater than:" : "Shorter than:"} />
-            <TextField
-                label="Type distance"
-                type={"number"}
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setChecked(event.target.checked)
+		handleFilters((prevState: any) => ({...prevState, distance_is_greater: event.target.checked}))
+	}
+	const handleDistanceChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setDistance(event.target.value)
+		handleFilters((prevState: any) => ({...prevState, covered_distance_m: event.target.value}))
+	}
+	return (
+		<Box display={"flex"} alignItems={"center"}>
+			<FormControlLabel control={<Checkbox checked={checked} onChange={handleChange}/>} label={checked ? "Greater than:" : "Shorter than:"} />
+			<TextField
+				label="Type distance"
+				type={"number"}
 
-                id="outlined-start-adornment"
-                sx={{ m: 1, width: '250px' }}
-                value={distance}
-                onChange={handleDistanceChange}
-                InputProps={{
-                    inputMode: 'numeric',
-                    endAdornment: <InputAdornment position="end">meters</InputAdornment>,
-                }}
-            />
-        </Box>
-    )
+				id="outlined-start-adornment"
+				sx={{ m: 1, width: "250px" }}
+				value={distance}
+				onChange={handleDistanceChange}
+				InputProps={{
+					inputMode: "numeric",
+					endAdornment: <InputAdornment position="end">meters</InputAdornment>,
+				}}
+			/>
+		</Box>
+	)
 }
 
 // @ts-ignore
 export const DurationFilter = ({handleFilters}) => {
-    const [duration, setDuration] = useState("0")
-    const [checked, setChecked] = React.useState(true);
+	const [duration, setDuration] = useState("0")
+	const [checked, setChecked] = React.useState(true)
 
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setChecked(event.target.checked);
-        handleFilters((prevState: any) => ({...prevState, duration_is_greater: event.target.checked}))
-    };
-    const handleDistanceChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setDuration(event.target.value);
-        handleFilters((prevState: any) => ({...prevState, duration_sec: event.target.value}))
-    };
-    return (
-        <Box display={"flex"} alignItems={"center"}>
-            <FormControlLabel control={<Checkbox checked={checked} onChange={handleChange}/>} label={checked ? "Greater than:" : "Shorter than:"} />
-            <TextField
-                label="Type duration"
-                type={"number"}
-                id="outlined-start-adornment"
-                sx={{ m: 1, width: '250px' }}
-                value={duration}
-                onChange={handleDistanceChange}
-                InputProps={{
-                    inputMode: 'numeric',
-                    endAdornment: <InputAdornment position="end">seconds</InputAdornment>,
-                }}
-            />
-        </Box>
-    )
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setChecked(event.target.checked)
+		handleFilters((prevState: any) => ({...prevState, duration_is_greater: event.target.checked}))
+	}
+	const handleDistanceChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setDuration(event.target.value)
+		handleFilters((prevState: any) => ({...prevState, duration_sec: event.target.value}))
+	}
+	return (
+		<Box display={"flex"} alignItems={"center"}>
+			<FormControlLabel control={<Checkbox checked={checked} onChange={handleChange}/>} label={checked ? "Greater than:" : "Shorter than:"} />
+			<TextField
+				label="Type duration"
+				type={"number"}
+				id="outlined-start-adornment"
+				sx={{ m: 1, width: "250px" }}
+				value={duration}
+				onChange={handleDistanceChange}
+				InputProps={{
+					inputMode: "numeric",
+					endAdornment: <InputAdornment position="end">seconds</InputAdornment>,
+				}}
+			/>
+		</Box>
+	)
 }
--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,45 +1,45 @@
-import React, {useEffect} from 'react';
-import mapboxgl from 'mapbox-gl';
-import {useSelector} from "react-redux";
-import {Station} from "../../store/actions/types";
-import CreateCustomPopup from "./CustomPopup";
-import {useNavigate} from "react-router-dom";
+import React, {useEffect} from "react"
+import mapboxgl from "mapbox-gl"
+import {useSelector} from "react-redux"
+import {Station} from "../../store/actions/types"
+import CreateCustomPopup from "./CustomPopup"
+import {useNavigate} from "react-router-dom"
 
 const Map = () => {
-    // @ts-ignore
-    const allStations = useSelector((state) => state.stations.allStations)
+	// @ts-ignore
+	const allStations = useSelector((state) => state.stations.allStations)
 
-    const navigate = useNavigate();
+	const navigate = useNavigate()
 
-    useEffect(() => {
-        mapboxgl.accessToken = 'pk.eyJ1IjoiaG9seWRvbmsiLCJhIjoiY2t3bTV4c2s5MXdqaTJ2bWxqYmNzeXg4ciJ9.9mHUItDBPKx0if6COMXKEg';
-        const map = new mapboxgl.Map({
-            container: 'map', // container ID
-            style: 'mapbox://styles/mapbox/light-v9', // style URL
-            center: [24.93, 60.16], // starting position [lng, lat]
-            zoom: 12, // starting zoom// display the map as a 3D globe
-        });
-        map.on('style.load', () => {
-            // @ts-ignore
-            map.setFog({}); // Set the default atmosphere style
-        });
-        allStations.forEach( (station : Station) => {
-            const popup = CreateCustomPopup(station.Osoite, station.Kapasiteet )
+	useEffect(() => {
+		mapboxgl.accessToken = "pk.eyJ1IjoiaG9seWRvbmsiLCJhIjoiY2t3bTV4c2s5MXdqaTJ2bWxqYmNzeXg4ciJ9.9mHUItDBPKx0if6COMXKEg"
+		const map = new mapboxgl.Map({
+			container: "map", // container ID
+			style: "mapbox://styles/mapbox/light-v9", // style URL
+			center: [24.93, 60.16], // starting position [lng, lat]
+			zoom: 12, // starting zoom// display the map as a 3D globe
+		})
+		map.on("style.load", () => {
+			// @ts-ignore
+			map.setFog({}) // Set the default atmosphere style
+		})
+		allStations.forEach( (station : Station) => {
+			const popup = CreateCustomPopup(station.Osoite, station.Kapasiteet )
 
-            const marker = new mapboxgl.Marker()
-                .setLngLat([station.x, station.y])
-                .setPopup(popup)
-                .addTo(map);
+			const marker = new mapboxgl.Marker()
+				.setLngLat([station.x, station.y])
+				.setPopup(popup)
+				.addTo(map)
 
-            marker.getElement().addEventListener('click', () => {
-            navigate(`/stations/${station.FID}`)
-            });
+			marker.getElement().addEventListener("click", () => {
+				navigate(`/stations/${station.FID}`)
+			})
 
-        })
-    }, [])
-    return (
-        <div id={'map'} style={{ width: '100%', height: '100vh'}}/>
-    );
-};
+		})
+	}, [])
+	return (
+		<div id={"map"} style={{ width: "100%", height: "100vh"}}/>
+	)
+}
 
-export default Map;
+export default Map
