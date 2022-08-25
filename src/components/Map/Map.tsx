efa8673a699a3ac7e549978ae189be66d14e2c1a
Stan_Effy <47476119+StanEffy@users.noreply.github.com>
2022-08-25 20:32:03 +0300

    change map component to react version

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,44 +1,50 @@
 import React, { useEffect } from "react"
-import mapboxgl from "mapbox-gl"
 import { useSelector } from "react-redux"
 import { Station, TState } from "../../store/actions/types"
-import CreateCustomPopup from "./CustomPopup"
 import { useNavigate } from "react-router-dom"
+import Map, { Marker } from "react-map-gl"
 
-const Map = () => {
+const MapComponent = () => {
 	const allStations = useSelector(
 		(state: TState) => state.stations.allStations
 	)
-
+	useEffect(() => {
+		console.log(allStations)
+	}, [])
 	const navigate = useNavigate()
 
-	useEffect(() => {
-		mapboxgl.accessToken =
-			"pk.eyJ1IjoiaG9seWRvbmsiLCJhIjoiY2t3bTV4c2s5MXdqaTJ2bWxqYmNzeXg4ciJ9.9mHUItDBPKx0if6COMXKEg"
-		const map = new mapboxgl.Map({
-			container: "map", // container ID
-			style: "mapbox://styles/mapbox/light-v9", // style URL
-			center: [24.93, 60.16], // starting position [lng, lat]
-			zoom: 12, // starting zoom// display the map as a 3D globe
-		})
-		map.on("style.load", () => {
-			// @ts-ignore
-			map.setFog({}) // Set the default atmosphere style
-		})
-		allStations.forEach((station: Station) => {
-			const popup = CreateCustomPopup(station.Osoite, station.Kapasiteet)
+	const handleClick = (id: number) => {
+		navigate(`/stations/${id}`)
+	}
 
-			const marker = new mapboxgl.Marker()
-				.setLngLat([station.x, station.y])
-				.setPopup(popup)
-				.addTo(map)
-
-			marker.getElement().addEventListener("click", () => {
-				navigate(`/stations/${station.ID}`)
-			})
-		})
-	}, [])
-	return <div id={"map"} style={{ width: "100%", height: "100vh" }} />
+	return (
+		<Map
+			initialViewState={{
+				longitude: 24.93,
+				latitude: 60.16,
+				zoom: 13,
+			}}
+			mapboxAccessToken={
+				"pk.eyJ1IjoiaG9seWRvbmsiLCJhIjoiY2t3bTV4c2s5MXdqaTJ2bWxqYmNzeXg4ciJ9.9mHUItDBPKx0if6COMXKEg"
+			}
+			style={{ width: "100%", height: "100vh" }}
+			mapStyle="mapbox://styles/mapbox/light-v9"
+		>
+			{allStations.map((station: Station) => {
+				console.log(station)
+				return (
+					<Marker
+						key={`marker-${station.Name}`}
+						longitude={station.x}
+						latitude={station.y}
+						anchor="bottom"
+						color="#76c2af"
+						onClick={() => handleClick(station.ID)}
+					/>
+				)
+			})}
+		</Map>
+	)
 }
 
-export default Map
+export default MapComponent
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,8 +1,7 @@
 export const pages = [
-	{ name: 'stations', href: '/stations' },
+	{ name: "stations", href: "/stations" },
 	// {name:'statistics', href: '/statistics'},
-	{ name: 'journeys', href: '/journeys' },
-	// {name:'add station', href: '/add_station'},
+	{ name: "journeys", href: "/journeys" },
 	// {name:'add trip', href: '/add_trip'},
-	{ name: 'map', href: '/map' },
+	{ name: "map", href: "/map" },
 ]
