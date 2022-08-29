3441090c7a7234b06f03e0b39d79835c0aa8a027
Stan_Effy <47476119+StanEffy@users.noreply.github.com>
2022-08-29 18:10:09 +0300

    refactor journeys list + create folder for utils functions

--- src/components/JourneysList/JourneysList.tsx
+++ src/components/JourneysList/JourneysList.tsx
@@ -9,8 +9,9 @@ import TripsFromStationTable from "../SingleStation/TripsFromStation"
 import { useDispatch, useSelector } from "react-redux"
 import { LoadFilteredTrips } from "../../store/actions/tripsAction"
 import { Station, TState } from "../../store/actions/types"
+import createJourneysQueryString from "../../utils/functions/createJourneysQueryString"
 
-type TFilter = {
+export type TFilter = {
 	departure_station_id: null | Station
 	return_station_id: null | Station
 	covered_distance_m: string | 0
@@ -33,36 +34,10 @@ const JourneysList = () => {
 		duration_is_greater: true,
 	})
 
-	const createQueryString = (filter: TFilter) => {
-		let str = ""
-		if (filter.departure_station_id !== null) {
-			str += "departure_station_id=" + filter.departure_station_id?.ID
-		}
-		if (filter.return_station_id !== null) {
-			filter.departure_station_id !== null
-				? (str += "&return_station_id=" + filter.return_station_id?.ID)
-				: (str += "return_station_id=" + filter.return_station_id?.ID)
-		}
-		if (filter.covered_distance_m > 0) {
-			if (filter.distance_is_greater) {
-				str += "&covered_distance_m[gte]=" + filter.covered_distance_m
-			} else {
-				str += "&covered_distance_m[lte]=" + filter.covered_distance_m
-			}
-		}
-		if (filter.duration_sec > 0) {
-			if (filter.duration_is_greater) {
-				str += "&duration_sec[gte]=" + filter.duration_sec
-			} else {
-				str += "&duration_sec[lte]=" + filter.duration_sec
-			}
-		}
-		return str
-	}
 	const handleFilter = () => {
 		console.log(toggleDisable())
 		// @ts-ignore
-		dispatch(LoadFilteredTrips(createQueryString(filters)))
+		dispatch(LoadFilteredTrips(createJourneysQueryString(filters)))
 	}
 
 	const toggleDisable = () => {
