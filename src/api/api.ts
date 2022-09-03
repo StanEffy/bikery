6f31eae10e5894d9d834742eacec25b9a26b4b40
Stan_Effy <47476119+StanEffy@users.noreply.github.com>
2022-09-03 23:23:29 +0300

    try one more time

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,16 +1,16 @@
 import axios from "axios"
 
 export const apiStations = axios.create({
-	baseURL: "http://52.47.198.242/api/v1/stations/",
+	baseURL: "https://52.47.198.242/api/v1/stations/",
 	"Access-Control-Allow-Credentials": true,
 })
 
 export const apiStationsStats = axios.create({
-	baseURL: "http://52.47.198.242/api/v1/stats/",
+	baseURL: "https://52.47.198.242/api/v1/stats/",
 	"Access-Control-Allow-Credentials": true,
 })
 
 export const apiTrips = axios.create({
-	baseURL: "http://52.47.198.242/api/v1/trips/",
+	baseURL: "https://52.47.198.242/api/v1/trips/",
 	"Access-Control-Allow-Credentials": true,
 })
