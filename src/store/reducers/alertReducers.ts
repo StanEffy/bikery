b442001031425305bdc3585babba2a3c49542f2e
Stan Effy <holydonk@gmail.com>
2023-01-09 22:19:01 +0200

    alert actions done

--- src/store/reducers/alertReducers.ts
+++ src/store/reducers/alertReducers.ts
@@ -1,31 +1,36 @@
-import {
-	ActionTypesAlert
-} from "../actions/types"
+import { ActionTypesAlert, ISetAlert, INullifyAlert } from "../actions/types"
 import { AlertColor } from "@mui/material"
 
-export type TAlertState = {
+export type TAlert = {
 	type: AlertColor
-	visibility: boolean
 	message: string
 	status?: string
 }
 
+export type TAlertState = {
+	alert: TAlert | null
+
+	visibility: boolean
+}
+
 export const initialState: TAlertState = {
-	type: "info",
-	message: "initial message",
+	alert: null,
 	visibility: false,
 }
-type TAction = ILoadAllTripsByStation | IAddNewTrip
+type TAction = ISetAlert | INullifyAlert
 
 export const tripsReducers = (state = initialState, action: TAction) => {
 	switch (action.type) {
 		case ActionTypesAlert.SetAlert: {
-			const {type, message} = action.payload
-			return { ...state, type, message}
-		}
-		case ActionTypesAlert.NullifyAlert {
 			return {
-				...initialState
+				...state,
+				alert: action.payload,
+				visibility: true,
+			}
+		}
+		case ActionTypesAlert.NullifyAlert: {
+			return {
+				...initialState,
 			}
 		}
 		default:
