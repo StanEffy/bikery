d3cf75911423cb864f967794db9b821aa470bd19
Stan Effy <holydonk@gmail.com>
2023-01-13 12:57:07 +0200

    rename handle error to handleAlert

--- src/utils/functions/handleAlert.ts
+++ src/utils/functions/handleAlert.ts
@@ -1,11 +1,11 @@
 import { TAlert } from "../../store/reducers/alertReducers"
 
 // eslint-disable-next-line no-empty-pattern
-const handleError = ({}): TAlert => {
+const handleAlert = ({}): TAlert => {
 	return {
 		type: "success",
 		message: "message",
 	}
 }
 
-export default handleError
+export default handleAlert
