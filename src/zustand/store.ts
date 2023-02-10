78e841c289e49e685a40d50530d1fff00908749a
Stan Effy <holydonk@gmail.com>
2023-02-10 18:47:52 +0200

    station store done

---
+++ src/zustand/store.ts
@@ -0,0 +1,30 @@
+import { create } from "zustand"
+import {
+	IStationToStationStats,
+	Station,
+	StationStats,
+} from "../store/actions/types"
+
+type State = {
+	allStations: Station[]
+	activeStation: null | Station
+	allStationsStats: [StationStats] | []
+	popularStations: IStationToStationStats[]
+}
+
+type Actions = {}
+
+const useStore = create<State & Actions>((set) => ({
+	allStations: [],
+	activeStation: null,
+	allStationsStats: [],
+	popularStations: [],
+	loadAllStations: (stations: Station[]) =>
+		set(() => ({ allStations: stations })),
+	loadAllStationsStats: (allStationsStats: [StationStats]) =>
+		set(() => ({ allStationsStats: allStationsStats })),
+	loadStationPopular: (popularStations: IStationToStationStats[]) =>
+		set(() => ({ popularStations: popularStations })),
+	setActiveStation: (activeStation: Station) =>
+		set(() => ({ activeStation })),
+}))
